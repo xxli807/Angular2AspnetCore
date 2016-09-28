a7d1fbb8b3e4ac7d9dfe0884f57cec8f2b588713
lxx871030@gmail.com <lxx871030@gmail.com>
2016-09-28 22:57:40 +1000

    add some dumy logic

--- Client/app/app.component.js
+++ Client/app/app.component.js
@@ -9,13 +9,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var hero_1 = require('./hero');
 var AppComponent = (function () {
     function AppComponent() {
+        this.heroes = [
+            new hero_1.Hero(1, 'batman'),
+            new hero_1.Hero(2, 'superman'),
+            new hero_1.Hero(3, 'roaddog')
+        ];
+        this.title = 'Title binding';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular App lol</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My favorite hero is:</h2>\n    <ul>\n      <li *ngFor=\"let hero of heroes\">\n        {{hero.id}}: {{ hero.name }}\n      </li>\n    </ul>\n    <p *ngIf=\"heroes.length > 2\">There are more than 2 heroes!</p>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
